cc19cacc8b8b295914cf8084701a548f4ac2bcb3
Mustapha7018 <adamsmustapha419@gmail.com>
2024-06-16 18:25:24 +0000

    add allowed hosts

--- video/settings.py
+++ video/settings.py
@@ -16,7 +16,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
